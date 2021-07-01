4dbba200c9190cac573321c37d4bd177232269a6
James McCarthy <james.joseph.mccarthy.905@gmail.com>
2021-07-01 17:54:06 -0400

    Updated Walkthrough

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,5 @@
 # """ 
-# The api package is defined by the api directory and the __init__.py script, and is referenced in the from api import routes statement. The app variable is defined as an instance of class Flask in the __init__.py script, which makes it a member of the api package.
-
-# The routes module is imported at the bottom and not at the top of the script as it is always done. The bottom import is a workaround to circular imports, a common problem with Flask applications. You are going to see that the routes module needs to import the app variable defined in this script, so putting one of the reciprocal imports at the bottom avoids the error that results from the mutual references between these two files. 
+# The api package is defined by the api directory and the __init__.py script, and is referenced in the from api import routes statement. The app variable is defined as an instance of class Flask in the __init__.py script, which makes it a member of the api package. 
 # """
 
 from flask import Flask
